5dd06772f425380d2fb70d69321924a271f7cdaf
Matt Newman <mdjnewman@gmail.com>
2016-05-13 19:31:18 +1000

    Adding Brisbane Project Managers & People Movers

--- src/weekly_meetups/core.clj
+++ src/weekly_meetups/core.clj
@@ -45,6 +45,7 @@
               "Cryptohack-Melbourne"
               "SecTalks-Brisbane"
               "NextBankBrisbane"
+              "Brisbane-Project-Management"
               "Ethereum-Brisbane"]
    :sydney []
    :melbourne []
